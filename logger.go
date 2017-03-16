99e790225d527c5315dc81b3b3e63dc46ac8a870
mralves <mralves@stone.com.br>
2017-03-16 00:02:19 -0300

    Adiciona recorrenca para mudança do nome do arquivo todo o dia

--- logger.go
+++ logger.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"time"
+	"sync"
 )
 
 type Logger interface {
@@ -13,6 +14,20 @@ type Logger interface {
 	Warn(message string, args ...interface{})
 	Error(message string, args ...interface{})
 	Fatal(message string, args ...interface{})
+}
+
+func init() {
+	go func() {
+		for {
+			today := time.Now()
+			to := today.Add(24 * time.Hour)
+			to = time.Date(to.Year(), to.Month(), to.Day(), 0, 0, 0, 0, to.Location())
+			diff := to.Sub(today) + (time.Duration(50) * time.Millisecond)
+			time.Sleep(diff)
+			SetOutputPerTime("")
+		}
+
+	}()
 }
 
 const (
@@ -45,11 +60,18 @@ func SetOutput(file string) error {
 	return nil
 }
 
-func SetOutputPerTime(fileNameWithoutExtension string) error {
+var filename string
+var lock = new(sync.RWMutex)
 
+func SetOutputPerTime(fileNameWithoutExtension string) error {
+	lock.Lock()
+	defer lock.Unlock()
+	if filename == "" {
+		filename = fileNameWithoutExtension
+	}
 	now := time.Now()
 
-	logName := fileNameWithoutExtension + now.Format("2006-01-02") + ".log"
+	logName := filename + now.Format("2006-01-02") + ".log"
 
 	logFile, err := os.OpenFile(logName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
@@ -63,24 +85,30 @@ func SetOutputPerTime(fileNameWithoutExtension string) error {
 }
 
 func (l logger) Debug(message string, args ...interface{}) {
-	l.logMessage(debug, message, args...)
+	go l.logMessage(debug, message, time.Now(), args...)
+	time.Sleep(5 * time.Microsecond)
 }
 func (l logger) Info(message string, args ...interface{}) {
-	l.logMessage(info, message, args...)
+	go l.logMessage(info, message, time.Now(), args...)
+	time.Sleep(5 * time.Microsecond)
 }
 func (l logger) Warn(message string, args ...interface{}) {
-	l.logMessage(warn, message, args...)
+	go l.logMessage(warn, message, time.Now(), args...)
+	time.Sleep(5 * time.Microsecond)
 }
 func (l logger) Error(message string, args ...interface{}) {
-	l.logMessage(Error, message, args...)
-
+	go l.logMessage(Error, message, time.Now(), args...)
+	time.Sleep(5 * time.Microsecond)
 }
 func (l logger) Fatal(message string, args ...interface{}) {
-	l.logMessage(fatal, message, args...)
+	go l.logMessage(fatal, message, time.Now(), args...)
+	time.Sleep(5 * time.Microsecond)
 }
 
-func (l logger) logMessage(level string, message string, args ...interface{}) {
-	logMes := fmt.Sprintf("%s||%s||%s||%s", time.Now().Format(time.RFC3339), l.owner, level, message)
+func (l logger) logMessage(level string, message string, t time.Time, args ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+	logMes := fmt.Sprintf("%s||%s||%s||%s", t.Format(time.RFC3339), l.owner, level, message)
 	if len(args) > 0 {
 		logMes += "||["
 		for i, arg := range args {
